321ae9536325bc542c4aa0f1b6f92db80f5db32c
Samuel Matheus <samuel.mrocha@ufrpe.br>
2026-04-23 20:34:46 -0300

    Organizando arquivos na pasta data

--- src/database.py
+++ src/database.py
@@ -1,24 +1,6 @@
 import json
 import os
 
-class Database:
-    FILE_PATH = "easyfinance_db.json"
-
-    @staticmethod
-    def carregar():
-        if not os.path.exists(Database.FILE_PATH):
-            return {"usuarios": [], "transacoes": [], "contas": []}
-        try:
-            with open(Database.FILE_PATH, 'r', encoding='utf-8') as f:
-                return json.load(f)
-        except Exception:
-            # Caso o arquivo esteja corrompido, retorna estrutura limpa (RF008)
-            return {"usuarios": [], "transacoes": [], "contas": []}
-
-    @staticmethod
-    def salvar(dados):
-        try:
-            with open(Database.FILE_PATH, 'w', encoding='utf-8') as f:
-                json.dump(dados, f, indent=4, ensure_ascii=False)
-        except Exception as e:
-            print(f"Erro crítico ao salvar dados: {e}")
+# Dentro de src/database.py
+CAMINHO_USUARIOS = "data/usuarios.csv"
+CAMINHO_TRANSACOES = "data/transacoes.csv"
